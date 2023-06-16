19912eb242b614c6cf8172056fca948e71258fa6
ad-1106 <adhyatmadeeptodi@gmail.com>
2023-06-16 22:16:57 +0545

    Made admin page less verbose and wasier to read

--- website/website/main_app/models.py
+++ website/website/main_app/models.py
@@ -14,20 +14,33 @@ class Class(models.Model):
     class_name=models.CharField(max_length=16)
     def __str__(self) :
         return f"{self.class_name}"
+    class Meta:
+        verbose_name = "Add Class in TC"
+        verbose_name_plural = "Add Class in TC"
 class principals_message(models.Model):
     message=models.TextField()
     image=models.ImageField(upload_to='website/images/' ,blank=False ,null=False)
-
+    def __str__(self):
+        return "Principals Message"
+    class Meta:
+        verbose_name = "Add  Principals message"
+        verbose_name_plural = "Add Principals message"
 class Alumni(models.Model):
     name=models.CharField(max_length=64)
     description_of_acheivement=models.TextField(max_length=128)
     def __str__(self):
         return f"{self.name}"
+    class Meta:
+        verbose_name = "Add Alumni"
+        verbose_name_plural = "Add Alumni"
 class News_letter(models.Model):
     title=models.CharField(max_length=32)
     file=models.FileField(upload_to='website/files',blank=False,null=False)
     def __str__(self) :
         return f"{self.title}"
+    class Meta:
+        verbose_name = "Make News Letter Entry"
+        verbose_name_plural = "Make News Letter Entry"
 class TC(models.Model):
     serial_number=models.BigIntegerField(primary_key=True,unique=True)
     name=models.CharField(max_length=32,blank=False,null=False)
@@ -39,39 +52,60 @@ class TC(models.Model):
     issue_date=models.DateField(blank=False,null=False,)
     def __str__(self):
         return f"{self.name}"
+    class Meta:
+        verbose_name = "Add TC"
+        verbose_name_plural = "Add TC"
 class vmc_member(models.Model):
     name=models.CharField(max_length=128)
     post=models.TextField()
     address=models.CharField(max_length=128)
     def __str__(self):
         return f"{self.name}"
+    class Meta:
+        verbose_name = "Add VMC member"
+        verbose_name_plural = "Add VMC member"
 class member_list(models.Model):
     name=models.CharField(max_length=32)
     designation=models.CharField(choices=Vacancy_posts,max_length=32)
     origin=models.CharField(choices=[("Local","Local"),("Indian","Indian")],null=False,blank=False,max_length=6)
     def __str__(self):
         return f"{self.name},{self.designation}"
+    class Meta:
+        verbose_name = "Add Staff"
+        verbose_name_plural = "Add Staff"
 class Vacancy(models.Model):
     number=models.IntegerField()
     vacant_designation=models.CharField(choices=Vacancy_posts,max_length=32)
     def __str__(self):
         return f"{self.vacant_designation}"
+    class Meta:
+        verbose_name = "Add Vacancy"
+        verbose_name_plural = "Add Vacancy"
 class Notice(models.Model):
     file=models.FileField(upload_to='website/files',blank=False,null=False)
     title=models.TextField(blank=False)
     description=models.TextField(blank=False)
     def __str__(self):
         return f"{self.title}"
+    class Meta:
+        verbose_name = "Add Notice"
+        verbose_name_plural = "Add Notice"
 class News_and_Events(models.Model):
     id=models.IntegerField(primary_key=True,auto_created=True,blank=False,null=False)
     title=models.TextField(blank=False)
     description=models.TextField(blank=False)
     def __str__(self):
         return f"{self.title}"
+    class Meta:
+        verbose_name = "Add Event Item"
+        verbose_name_plural = "Add Event Item"
 class Fee_structure(models.Model):
     file=models.FileField(upload_to='website/files',blank=False,null=False)
     def __str__(self):
         return f"Fee structure for the year {date.today().year}"
+    class Meta:
+        verbose_name = "Add Fee Structure"
+        verbose_name_plural = "Add Fee Structure"
 class Holiday(models.Model):
     name=models.TextField(max_length=32,blank=False,null=False)
     start_date=models.DateField(blank=False,null=False)
@@ -81,24 +115,31 @@ class Holiday(models.Model):
                               max_length=32)
     def __str__(self):
         return f"{self.name}"
+    class Meta:
+        verbose_name = "Add Staff"
+        verbose_name_plural = "Add Staff"
 class Carousel_image(models.Model):
-        image_1=models.ImageField(upload_to='website/images/' ,blank=False ,null=False)
-        image_2=models.ImageField(upload_to='website/images/' ,blank=False ,null=False)
-        image_3=models.ImageField(upload_to='website/images/' ,blank=False ,null=False)
-        image_4=models.ImageField(upload_to='website/images/' ,blank=False ,null=False)
+    image_1=models.ImageField(upload_to='website/images/' ,blank=False ,null=False)
+    image_2=models.ImageField(upload_to='website/images/' ,blank=False ,null=False)
+    image_3=models.ImageField(upload_to='website/images/' ,blank=False ,null=False)
+    image_4=models.ImageField(upload_to='website/images/' ,blank=False ,null=False)
+    class Meta:
+        verbose_name = "Add Staff"
+        verbose_name_plural = "Add Staff"
 class News_and_Events_GalleryAdmin(admin.ModelAdmin):
     exclude= ('id',)
 class quote(models.Model):
     quote=models.TextField()
     last_updated=models.DateField()
     author=models.TextField()
-
 class Gallery(models.Model):
     id=models.IntegerField(primary_key=True,auto_created=True,blank=False,null=False)
     title=models.TextField(blank=False)
     def __str__(self):
         return f"{self.title}"
-    
+    class Meta:
+        verbose_name = "Add Gallery Item"
+        verbose_name_plural = "Add Gallery Item"
 class Images(models.Model):
     event=models.ForeignKey(News_and_Events,on_delete=models.CASCADE,related_name='images',blank=True,null=True)
     gallery=models.ForeignKey(Gallery,on_delete=models.CASCADE,related_name='images',blank=True,null=True)
@@ -108,6 +149,12 @@ class Images(models.Model):
             return f"{self.event}"
         else:
             return f"{self.gallery}"
+    class Meta:
+        verbose_name = "Add Image For gallery and event"
+        verbose_name_plural = "Add Image For gallery and event"
 class Committies(models.Model):
     Committee_name=models.TextField(choices=(('Admissions','Admissions'),('Academic advisory','Academic_Advisory'),('Examination','Examination'),('Transpotation','Transpotation')))
     members=models.ManyToManyField(member_list,related_name='committees')
+    class Meta:
+        verbose_name = "Update Committee Member"
+        verbose_name_plural = "Update Committee Member"
