c27ea0645aacaa7165f0caf4926db425b3d71ded
ad-1106 <adhyatmadeeptodi@gmail.com>
2023-07-02 22:06:44 +0545

    added title to achievement,removed milestone.html and added 6 events to homepage

--- website/website/main_app/views.py
+++ website/website/main_app/views.py
@@ -38,7 +38,7 @@ def index(request):
             quote_to_be_displayed=quotes.quote
             author=quotes.author
         notice=Notice.objects.all()[::-1][:5]
-        events=News_and_Events.objects.all()[::-1][:4]
+        events=News_and_Events.objects.all()[::-1][:6]
         carousel_image=Carousel_image.objects.get()
         return render(request,"website/index.html",{
             "notice":notice,
