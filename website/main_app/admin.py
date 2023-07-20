10f857493867f43a1cd7d1d90bb5e8730e833210
ad-1106 <adhyatmadeeptodi@gmail.com>
2023-07-20 19:13:33 +0545

    made the file thing better

---
+++ website/main_app/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+admin.site.register(TC)
+admin.site.register(principals_message)
+admin.site.register(Holiday)
+admin.site.register(Alumni)
+admin.site.register(Class)
+admin.site.register(News_letter)
+admin.site.register(vmc_member)
+admin.site.register(member_list)
+admin.site.register(Vacancy)
+admin.site.register(Fee_structure)
+admin.site.register(Carousel_image)
+admin.site.register(News_and_Events,News_and_Events_Admin)
+admin.site.register(Notice,Notice_Admin)
+admin.site.register(Images)
+admin.site.register(Committies)
+admin.site.register(Admissions)
+admin.site.register(class1)
+admin.site.register(class11)
+admin.site.register(achievement)
---
+++ website/main_app/static/website/index.js
@@ -0,0 +1,31 @@
+let toTopBtn = document.getElementById('scroll-to-top');
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 20 || document.body.scrollTop >20) {
+        toTopBtn.style.display = "block";
+    } else {
+        toTopBtn.style.display = "none";
+    }
+}
+
+toTopBtn.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0
+    })
+})
+
+window.addEventListener('resize', () => { 
+    var element = document.getElementById('navbarNav');
+    if (window.innerWidth<=992) {
+        element.classList.remove('justify-content-end');
+        element.classList.add('justify-content-center');
+    } else {
+        element.classList.remove('justify-content-center');
+        element.classList.add('justify-content-end');
+    }
+})
+
+//enable popover
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
